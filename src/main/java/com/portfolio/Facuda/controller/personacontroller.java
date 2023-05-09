cca4647f4a07891cfd8b6ddda30237ef2f24295d
FacuDA <facundoaguirre9488@gmail.com>
2023-05-09 14:10:50 -0300

    intento_1

--- src/main/java/com/portfolio/Facuda/controller/personacontroller.java
+++ src/main/java/com/portfolio/Facuda/controller/personacontroller.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -87,4 +88,6 @@ public class personacontroller {
         
         return new ResponseEntity(new Mensaje("Persona actualizada"), HttpStatus.OK);
     }
+    
+  
 }
