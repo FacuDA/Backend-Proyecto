ba01102563ed788337b7c9caabef0f53d61ea0da
FacuDA <facundoaguirre9488@gmail.com>
2023-05-09 21:10:30 -0300

    prueba_final

--- src/main/java/com/portfolio/Facuda/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/Facuda/Security/Controller/AuthController.java
@@ -37,7 +37,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"https://facudafrontend.web.app","http://localhost:4200"})
+@CrossOrigin(origins = "https://facudafrontend.web.app")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
